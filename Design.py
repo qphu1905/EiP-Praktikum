5771347778c082c5b3170c8c6183feb5f59e9e30
Charlize1884 <cbololoi@students.uni-mainz.de>
2025-03-17 14:45:56 +0100

    add Design.py

---
+++ Design.py
@@ -0,0 +1,22 @@
+import pygame as pg
+
+pg.init()
+
+screen = pg.display.set_mode((1200, 700))
+pg.display.set_caption('Cave')
+
+background = pg.image.load('cave_wall2.jpg')
+game_active = True
+while game_active:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            game_active = False
+            print("Game Over")
+
+    screen.fill((0, 200, 0))
+
+    background = pg.transform.scale(background, (1200, 700))
+    screen.blit(background, (0, 0))
+    pg.display.flip()
+
+
