ebf7b868717cec4f9a0dc6dbd6483025bae92abc
qphu1905 <quobui@students.uni-mainz.de>
2025-03-18 10:06:36 +0100

    feat <main.py> Wrapped main code in main() function, added main guard.

--- main.py
+++ main.py
@@ -19,26 +19,31 @@ def load_img(filename):
 #def load_sound() if sound is added
 
 
-#pygame setup
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+def main():
+    #pygame setup
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 600
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-running = True
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
 
-while running:
-    if pygame.event.get(pygame.QUIT):
-        running = False
-        raise SystemExit
+    while running:
+        if pygame.event.get(pygame.QUIT):
+            running = False
+            raise SystemExit
 
-    #Logical update here
+        #Logical update here
 
-    screen.fill('black')
+        screen.fill('black')
 
-    #Render graphic here
+        #Render graphic here
 
-    pygame.display.flip()   #Flip display
-    clock.tick(60)          #Limit FPS: 60
-pygame.quit()
+        pygame.display.flip()   #Flip display
+        clock.tick(60)          #Limit FPS: 60
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
