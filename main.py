4c202ca4cbb270f45a3024d7c12cb271a3bf06dc
qphu1905 <quobui@students.uni-mainz.de>
2025-03-20 12:10:11 +0100

    feat <main.py> Add scroll functionality that follows player.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def load_img(filename):
 #def load_sound() if sound is added
 
 
-def load_map(level, resolution: int, sprite_entities):
+def load_map(level, resolution: int, sprite_groups: list):
     platforms = []
     y = 0
     for row in level:
@@ -32,15 +32,20 @@ def load_map(level, resolution: int, sprite_entities):
                 platform_rect = platform_loaded_image[1]
                 p = Platform(platform_image, platform_rect, x, y)
                 platforms.append(p)
-                p.add(sprite_entities)
+                for group in sprite_groups:
+                    p.add(group)
             x += resolution
         y += resolution
     return platforms
 
 def main():
+    #Game Parameter
+    ACC = 1
+    FRIC = -0.12
+
     #pygame setup
-    SCREEN_WIDTH = 960
-    SCREEN_HEIGHT = 640
+    SCREEN_WIDTH = 640
+    SCREEN_HEIGHT = 480
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -48,42 +53,59 @@ def main():
     running = True
 
     sprite_entities = pygame.sprite.Group()
+    platform_entities = pygame.sprite.Group()
     player_loaded_image = load_img('ninja_resized.png')
     player_image = player_loaded_image[0]
     player_rect = player_loaded_image[1]
     player = Player(player_image, player_rect, 480, 320, 100 )
     player.add(sprite_entities)
 
-    level = ['p             p',
-             'p             p',
-             'p             p',
-             'p             p',
-             'p             p',
-             'p             p',
-             'p             p',
-             'p             p',
-             'p             p',
-             'ppppppppppppppp',]
-    platforms = load_map(level, 64, sprite_entities)
+    level = ['     ppp                      ',
+             '                              ',
+             '                     ppp      ',
+             '              ppp             ',
+             '                              ',
+             '             ppp              ',
+             '                              ',
+             '     ppp                      ',
+             '                              ',
+             '                  ppp         ',
+             '       ppp                    ',
+             '                              ',
+             '    ppp           ppp         ',
+             '                              ',
+             '        ppp              ppp  ',
+             '                              ',
+             '                ppp           ',
+             '     ppp                      ',
+             '                              ',
+             'pppppppppppppppppppppppppppppp']
+
+    platforms = load_map(level, 32, [sprite_entities, platform_entities])
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 raise SystemExit
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.jump()
 
         #Logical update here
         dt = clock.tick(60) / 1000
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player.jump(dt)
-        if keys[pygame.K_a]:
-            player.moveleft(dt)
-        if keys[pygame.K_d]:
-            player.moveright(dt)
 
-        player.collision(platforms)
+        player.move(ACC, FRIC)
+        player.update(player, platform_entities)
 
+        if player.rect.top <= SCREEN_HEIGHT / 3:
+            player.pos.y += abs(player.velocity.y)
+            for p in platform_entities:
+                p.rect.y += abs(player.velocity.y)
+        elif player.rect.bottom >= SCREEN_HEIGHT / 4:
+            player.pos.y -= abs(player.velocity.y)
+            for p in platform_entities:
+                p.rect.y -= abs(player.velocity.y)
         screen.fill('black')
 
         #Render graphic here
