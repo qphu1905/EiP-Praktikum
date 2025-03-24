f3129c2349b05140559a4677cf12f2581bd50b39
qphu1905 <quobui@students.uni-mainz.de>
2025-03-24 10:54:58 +0100

    Add comment for clarity

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 import os
 from Player import Player
@@ -8,11 +10,13 @@ def load_img(filename):
     filepath = os.path.join('asset', filename)
     try:
         image = pygame.image.load(filepath)
+        width = image.get_width()
+        height = image.get_height()
         if image.get_alpha() is None:
             image = image.convert()
         else:
             image = image.convert_alpha()
-        return image, image.get_rect()
+        return image, image.get_rect(center=(width/2, height/2))
     except FileNotFoundError:
         print(f'File {filepath} found')
 
@@ -30,7 +34,7 @@ def load_map(level, resolution: int, sprite_groups: list):
                 platform_loaded_image = load_img('brickwall.png')
                 platform_image = platform_loaded_image[0]
                 platform_rect = platform_loaded_image[1]
-                p = Platform(platform_image, platform_rect, x, y)
+                p = Platform(platform_image, platform_rect)
                 platforms.append(p)
                 for group in sprite_groups:
                     p.add(group)
@@ -38,10 +42,18 @@ def load_map(level, resolution: int, sprite_groups: list):
         y += resolution
     return platforms
 
+
+def generate_platform(platform_entities, sprite_entities, SCREEN_HEIGHT, SCREEN_WIDTH):
+    while len(platform_entities) < 8:
+        width = random.randrange(50,100)
+        p = Platform(random.randint(10,SCREEN_WIDTH-10),
+                     random.randint(0, SCREEN_HEIGHT-10))
+        p.rect.center = (random.randrange(0, SCREEN_WIDTH - width),
+                         random.randrange(-50, 0))
+        p.add(sprite_entities)
+        p.add(platform_entities)
+
 def main():
-    #Game Parameter
-    ACC = 1
-    FRIC = -0.12
 
     #pygame setup
     SCREEN_WIDTH = 640
@@ -52,66 +64,97 @@ def main():
     clock = pygame.time.Clock()
     running = True
 
+    #Create sprite group for all object, and sprite group for platform
     sprite_entities = pygame.sprite.Group()
     platform_entities = pygame.sprite.Group()
+
+    #Load player image
     player_loaded_image = load_img('ninja_resized.png')
     player_image = player_loaded_image[0]
     player_rect = player_loaded_image[1]
+
+    #Create player sprite object
     player = Player(player_image, player_rect, 480, 320, 100 )
+    #Add player sprite object to sprite group for all object
     player.add(sprite_entities)
 
-    level = ['     ppp                      ',
-             '                              ',
-             '                     ppp      ',
-             '              ppp             ',
-             '                              ',
-             '             ppp              ',
-             '                              ',
-             '     ppp                      ',
-             '                              ',
-             '                  ppp         ',
-             '       ppp                    ',
-             '                              ',
-             '    ppp           ppp         ',
-             '                              ',
-             '        ppp              ppp  ',
-             '                              ',
-             '                ppp           ',
-             '     ppp                      ',
-             '                              ',
-             'pppppppppppppppppppppppppppppp']
+    #Create Platform object ground
+    #ground Platform is created seperated due to different color,
+    #non-randomized position, and moving parameter set to False
+    ground = Platform(0,0)
+    #Create surface for ground object that is half as wide as screen size and 16 pixel high
+    ground.image = pygame.Surface((SCREEN_WIDTH / 2, 16))
+    ground.image.fill('red')
+    ground.rect = ground.image.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 8))
+    #Disable ability for the ground to move
+    ground.moving = False
+    #Add ground to all sprites group, and platform sprites group
+    ground.add(sprite_entities)
+    ground.add(platform_entities)
 
-    platforms = load_map(level, 32, [sprite_entities, platform_entities])
+    #Create initial platforms, disallow moving for initial platforms
+    #4 or 5 platforms may be created to randomize start
+    for i in range(random.randint(4,5)):
+        p = Platform(random.randint(10,SCREEN_WIDTH-10),
+                     random.randint(0, SCREEN_HEIGHT-10))
+        p.moving = False
+        p.add(sprite_entities)
+        p.add(platform_entities)
 
+    #MAIN GAME LOOP
     while running:
         for event in pygame.event.get():
+            #Check quit event
             if event.type == pygame.QUIT:
                 running = False
                 raise SystemExit
+            #Check for jumping event
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE or event.key == pygame.K_w or event.key == pygame.K_UP:
                     player.jump()
 
-        #Logical update here
-        dt = clock.tick(60) / 1000
+        #LOGICAL UPDATE
+        #Iterate through all Platform entities, call move method to move platforms
+        for p in platform_entities:
+            p.move(SCREEN_WIDTH)
 
-        player.move(ACC, FRIC)
+        #Move player when input is detected
+        player.move()
+        #Perform collision check between player and platforms
         player.update(player, platform_entities)
 
+        #Generate platforms above the player, outside screen area
+        generate_platform(platform_entities, sprite_entities, SCREEN_HEIGHT, SCREEN_WIDTH)
+
+        #Scroll screen when player reached specific part of screen
         if player.rect.top <= SCREEN_HEIGHT / 3:
             player.pos.y += abs(player.velocity.y)
+            #Check if platform are below screen, delete if so
             for p in platform_entities:
-                p.rect.y += abs(player.velocity.y)
-        elif player.rect.bottom >= SCREEN_HEIGHT / 4:
-            player.pos.y -= abs(player.velocity.y)
-            for p in platform_entities:
-                p.rect.y -= abs(player.velocity.y)
+                p.rect.top += abs(player.velocity.y)
+                if p.rect.top >= SCREEN_HEIGHT:
+                    p.kill()
+        #Check if player fell below screen
+        elif player.rect.bottom >= SCREEN_HEIGHT:
+            print("You DIED")
+            raise SystemExit
+
+        #Allow player to go from one side of the screen to the other
+        if player.pos.x > SCREEN_WIDTH:
+            player.pos.x = 0
+            player.pos.y -= 10
+        if player.pos.x < 0:
+            player.pos.x = SCREEN_WIDTH
+            player.pos.y -= 10
         screen.fill('black')
 
         #Render graphic here
+        #Draw all sprites in general sprite group
         sprite_entities.draw(screen)
-        pygame.display.flip()   #Flip display
-        clock.tick(60)          #Limit FPS: 60
+        #Flip display
+        pygame.display.flip()
+        #Limit FPS: 60
+        clock.tick(60)
 
     pygame.quit()
 
